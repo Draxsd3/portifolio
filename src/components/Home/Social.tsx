531295af07addbbdc5705cd9cadb88dd8a8912fe
Renan Ramos <103542846+Draxsd3@users.noreply.github.com>
2025-07-17 17:19:53 -0300

    Update Social.tsx

--- src/components/Home/Social.tsx
+++ src/components/Home/Social.tsx
@@ -4,21 +4,21 @@ export default function Social() {
   return (
     <div className="home__social">
       <a
-        href="https://www.instagram.com/cleytin_1/"
+        href="https://www.instagram.com/mr.renan1/"
         className="home__social-icon"
         target="_blank"
       >
         <Instagram />
       </a>
       <a
-        href="https://github.com/Cleyton-Honorato"
+        href="https://github.com/Draxsd3/Draxsd3"
         className="home__social-icon"
         target="_blank"
       >
         <Github />
       </a>
       <a
-        href="https://www.linkedin.com/in/cleyton-honorato-a5586215a/"
+        href="https://www.linkedin.com/in/renan-ramos-068759243/"
         className="home__social-icon"
         target="_blank"
       >
